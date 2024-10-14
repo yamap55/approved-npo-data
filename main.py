cb83d8c859b24dab54161b641fa2aaa28df3a82d
yamap55 <1785817+yamap55@users.noreply.github.com>
2024-10-14 19:09:43 +0900

    chore: update main

--- main.py
+++ main.py
@@ -4,16 +4,11 @@ from logging import config, getLogger
 from pathlib import Path
 from time import perf_counter, sleep
 
-from tenacity import retry, stop_after_attempt, wait_random
-
 from approved_npo_data.all_npo_data import get_all_npo_data_from_url
 from approved_npo_data.approved_npo_data import get_approved_npo_data
 from approved_npo_data.config import MAX_ITEMS_TO_PROCESS, SCRAPING_DELAY_SECONDS
 from approved_npo_data.csv.csv_row import AllNpoDataRow, ApprovedNpoRow, OutputApprovedNpoRow
-from approved_npo_data.scraping.npoportal_detail.viewing_documents import scrape_viewing_documents
-from approved_npo_data.scraping.npoportal_detail.viewing_documents_model import (
-    FinancialActivityReport,
-)
+from approved_npo_data.scraping.npoportal_detail.npoportal_detail import get_detail_data
 from approved_npo_data.util.date_format import simple_format_time
 from approved_npo_data.util.enumerate import controlled_enumerate
 from approved_npo_data.util.file_operations import get_output_path, save_csv
@@ -24,21 +19,6 @@ logger = getLogger(__name__)
 
 BASE_PATH = Path(".")
 BASE_PATH.mkdir(parents=True, exist_ok=True)
-
-
-# 件数が多いからか割と失敗するためリトライを設定している
-@retry(stop=stop_after_attempt(3), wait=wait_random(min=1, max=10))
-def get_viewing_documents(url: str) -> tuple[str, str]:
-    """法人情報のURLから最新年度の財務活動報告書のURLを取得する"""
-    viewing_documents = scrape_viewing_documents(url)
-    financial_activity_report = viewing_documents.financial_activity_reports.get_latest_report()
-
-    def f(report: FinancialActivityReport):
-        urls = "\n".join(d.url for d in report.documents)
-        return str(report.year), urls
-
-    year, urls = f(financial_activity_report) if financial_activity_report else ("", "")
-    return year, urls
 
 
 def createOutputApprovedNpoRow(
@@ -87,20 +67,18 @@ def main():
         npoData = getNpoDataRow(associate_name)
         url = npoData.corporate_information_url
         # TODO: 詳細ページ情報
-        detail_page_data = ["", ""]
+        # 存在しない場合の空データもdataclassから取得する必要がある
+        detail_data = ["" for _ in range(23)]
         if url:
             try:
                 # URLが存在する場合には詳細ページからスクレイピングを行う
-                year, urls = get_viewing_documents(url)
-                detail_page_data = [year, urls]
+                detail_data = get_detail_data(url)
 
                 # スクレイピングの負荷を考慮してスリープを入れる
                 sleep(SCRAPING_DELAY_SECONDS)
             except Exception as e:
                 logger.error(f"{associate_name} のスクレイピングに失敗しました。{url} {e}")
-        outputApprovedNpoRow = createOutputApprovedNpoRow(
-            approved_npo_row, npoData, detail_page_data
-        )
+        outputApprovedNpoRow = createOutputApprovedNpoRow(approved_npo_row, npoData, detail_data)
         output_data.append(outputApprovedNpoRow)
     logger.info(f"end merge data {len(output_data)=}, {len(not_in_approve_npo)=}")
 
