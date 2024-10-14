cdeb566d0f1998a0218747d962885d8f7c6e91fe
yamap55 <1785817+yamap55@users.noreply.github.com>
2024-10-14 17:12:30 +0900

    chore: update config

--- approved_npo_data/config.py
+++ approved_npo_data/config.py
@@ -1,7 +1,8 @@
 """各種設定"""
 
 # スクレイピングの間隔（秒）
-SCRAPING_DELAY_SECONDS = 1
+SCRAPING_DELAY_SECONDS = 0.1
 
 # 処理件数（Noneの場合は全件）
-MAX_ITEMS_TO_PROCESS = 30
+# NOTE: テストのときは30件くらいが良いかもしれない
+MAX_ITEMS_TO_PROCESS = None
