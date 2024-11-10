bcc74a06f85ef2235a236033ec901fb360126e9e
yamap55 <1785817+yamap55@users.noreply.github.com>
2024-11-10 18:58:05 +0900

    feat: add tokyo detail page data

--- approved_npo_data/csv/csv_row.py
+++ approved_npo_data/csv/csv_row.py
@@ -674,3 +674,79 @@ class OutputApprovedNpoRow(ModelBase):
     # 詳細ページ 閲覧書類
     document_latest_year: str = field(default="", metadata={"key": "ドキュメント最新年度"})
     document_url: str = field(default="", metadata={"key": "ドキュメントURL"})
+
+    # 東京都の法人・団体情報
+    tokyo_corporate_number: str = field(default="", metadata={"key": "法人番号"})
+    """法人番号"""
+
+    tokyo_approval_date: str = field(default="", metadata={"key": "認証日"})
+    """認証日"""
+
+    tokyo_corporate_name: str = field(default="", metadata={"key": "法人・団体名称"})
+    """法人・団体名称"""
+
+    tokyo_corporate_name_kana: str = field(default="", metadata={"key": "法人・団体名称カナ"})
+    """法人・団体名称カナ"""
+
+    tokyo_main_office_address: str = field(default="", metadata={"key": "主たる事務所の所在地"})
+    """主たる事務所の所在地"""
+
+    tokyo_secondary_office_address: str = field(
+        default="", metadata={"key": "従たる事務所の所在地", "optional": True}
+    )
+    """
+    従たる事務所の所在地
+
+    ※存在しない場合があるため、optional=Trueと指定している
+    """
+
+    tokyo_representative_name: str = field(default="", metadata={"key": "代表者氏名"})
+    """代表者氏名"""
+
+    tokyo_articles_of_incorporation_purpose: str = field(
+        default="", metadata={"key": "定款に記載された目的"}
+    )
+    """定款に記載された目的"""
+
+    tokyo_activity_fields: str = field(default="", metadata={"key": "活動分野"})
+    """活動分野"""
+
+    tokyo_phone_number: str = field(default="", metadata={"key": "電話番号"})
+    """電話番号"""
+
+    tokyo_fiscal_year: str = field(default="", metadata={"key": "事業年度"})
+    """事業年度"""
+
+    tokyo_approval_status: str = field(default="", metadata={"key": "認定状態"})
+    """認定状態"""
+
+    tokyo_approval_date_for_exception: str = field(
+        default="", metadata={"key": "認定（特例認定）日", "optional": True}
+    )
+    """
+    認定（特例認定）日
+
+    ※存在しない場合があるため、optional=Trueと指定している
+    """
+
+    tokyo_validity_period: str = field(default="", metadata={"key": "有効期間"})
+    """有効期間"""
+
+    tokyo_dissolution_date: str = field(default="", metadata={"key": "解散日", "optional": True})
+    """
+    解散日
+
+    ※存在しない場合があるため、optional=Trueと指定している
+    """
+
+    tokyo_dissolution_reason: str = field(
+        default="", metadata={"key": "解散理由", "optional": True}
+    )
+    """
+    解散理由
+
+    ※存在しない場合があるため、optional=Trueと指定している
+    """
+
+    tokyo_documents: str = field(default="", metadata={"key": "閲覧書類"})
+    """閲覧書類"""
